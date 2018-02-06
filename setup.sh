ad221f1c93b89393b72c65984abc8b3e0f6c4cf4
artlyhub <ppark9553@gmail.com>
2018-02-06 15:32:30 +0900

    adding bash scripts

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
 #! /bin/bash
+
+## change owndership of celerybeat-schedule file
+chown arbiter celerybeat-schedule
+
+## copy celery conf files to supervisor conf directory
+cp ./config/celery* /etc/supervisor/conf.d
+
+## read in supervisor conf files and start processes
+supervisorctl reread
+supervisorctl update
+supervisorctl start arbiter_celery
+supervisorctl start arbiter_celerybeat
