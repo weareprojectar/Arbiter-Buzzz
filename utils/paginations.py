2b61460ede82455870b3f066c8e53d54ad881391
artlyhub <ppark9553@gmail.com>
2018-02-09 03:36:46 +0900

    changed pagination to 500 for ohlcv

--- utils/paginations.py
+++ utils/paginations.py
@@ -13,6 +13,6 @@ class StandardResultPagination(pagination.PageNumberPagination):
     # max_page_size = 1000
 
 class OHLCVPagination(pagination.PageNumberPagination):
-    page_size = 1000
+    page_size = 500
     page_size_query_param = 'page_size'
     # max_page_size = 1000
