8740aa376ec9a839ffa285f012de74a2af788a11
artlyhub <ppark9553@gmail.com>
2018-02-21 11:14:03 +0900

    adding db_backup.sh

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -2,3 +2,6 @@
 
 ### 1. BM
 su arbiter -c "psql -c \"\copy stockapi_bm (date, name, index, volume, individual, foreigner, institution) to '/home/arbiter/backup/bm.csv' delimiter ',';\""
+echo BM backup successful
+
+### 2. 
